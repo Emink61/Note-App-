65e9dafcc9a9fbffb21acef7fd8a59783dba9a62
Emin <eminkilic1223@gmail.com>
2026-02-22 20:40:31 +0300

    Enhance note app with input validation and item check
    Added input validation and click event for list items.

--- note app/app.js
+++ note app/app.js
@@ -3,19 +3,22 @@ const button = document.getElementById("addNoteBtn");
 let list = document.getElementById("noteList");
 
 function appendmessage() {
+    if (message.value.trim() === "") return;
     let listElement = document.createElement("li");
     listElement.textContent = message.value;
 
     const delbutton = document.createElement("button");
     delbutton.textContent = "X";
     delbutton.classList.add("delete-btn");
-
+    listElement.addEventListener("click",function() {
+        listElement.classList.add("checked");
+    });
     delbutton.addEventListener("click", function(){
-
         listElement.classList.add("smoke-animation")
         setTimeout(() => {
             listElement.remove();
         }, 700);
+
 
         delbutton.remove();
     });
